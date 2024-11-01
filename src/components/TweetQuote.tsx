aab57b304a0c2ab9dc3eab53a5f6792d29762253
Hongyu Yang <brutusyhy@gmail.com>
2024-11-01 16:05:24 +0300

    Minor changes so that it passed all fCC tests

--- src/components/TweetQuote.tsx
+++ src/components/TweetQuote.tsx
@@ -1,5 +1,6 @@
 export default function tweetQuote({text, author}: {text: string; author: string}) {
-    return (<a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(text+"\n"+author)}`}>
+    return (<a id="tweet-quote"
+        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(text+"\n"+author)}`}>
         Tweet this quote
     </a>);
 }
